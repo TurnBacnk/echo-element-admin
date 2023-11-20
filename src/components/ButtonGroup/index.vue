7b384e4abc8e0110a369439fb3f04947676b55ff
roadwang <1814127606@qq.com>
2023-11-20 09:42:27 +0800

    按钮组组件条

---
+++ src/components/ButtonGroup/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <el-row :gutter="10" class="mb8">
+    <template v-for="button in buttonConfig">
+      <el-col :span="1.5">
+        <el-button
+          :circle="button.circle"
+          :icon="button.icon ? button.icon : ''"
+          :plain="button.plain ? button.plain : false"
+          :size="button.size ? button.size : 'mini'"
+          :type="button.type ? button.type : 'primary'"
+          @click="button.click()"
+        >{{ button.text }}
+        </el-button>
+      </el-col>
+    </template>
+  </el-row>
+</template>
+
+<script>
+
+export default {
+  name: 'ButtonGroup',
+  props: {
+    buttonConfig: {
+      type: Array,
+      required: true,
+      default: [
+        {
+          text: '按钮',
+          type: 'primary',
+          size: 'mini',
+          click: () => {
+            console.log('click success')
+          },
+          plain: true,
+          icon: ''
+        }
+      ]
+    }
+  },
+  data() {
+    return {}
+  },
+  methods: {}
+}
+</script>
